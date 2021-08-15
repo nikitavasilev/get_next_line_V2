6195915e67af950c12d0a62bc06283f552deb80c
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-08-16 00:21:36 +0200

    ++ strdup

---
+++ get_next_line_utils.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/15 04:19:10 by nvasilev          #+#    #+#             */
+/*   Updated: 2021/08/15 05:14:33 by nvasilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+
+	if (!s1 || !s2)
+		return (0);
+	if (!(str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
+		return (0);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		str[i + ft_strlen(s1)] = s2[i];
+		i++;
+	}
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	return (str);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	char	*copy;
+
+	if (!(copy = malloc(ft_strlen(s1) + 1)))
+		return (0);
+	i = 0;
+	while (s1[i])
+	{
+		copy[i] = s1[i];
+		i++;
+	}
+	copy[i] = s1[i];
+	return (copy);
+}
