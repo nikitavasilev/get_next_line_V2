6ffb7b41cbfb7efabf9714828dbc31404716c5b4
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-08-18 16:25:11 +0200

    bool true false

--- get_next_line.h
+++ get_next_line.h
@@ -6,24 +6,29 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/15 03:22:45 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/08/15 05:15:36 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/08/18 16:03:59 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdio.h>
+typedef enum {FALSE, TRUE} boolean;
+
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 32
 # endif
 
 char	*get_next_line(int fd);
+
+// utils functions
+size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
 
 #endif
