f74faa687a963646c568ac1dcea844b1f2a16509
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-08-18 16:32:53 +0200

    norm enum

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/15 03:22:45 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/08/18 16:03:59 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/08/18 16:32:01 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-typedef enum {FALSE, TRUE} boolean;
-
 # include <stdlib.h>
 # include <unistd.h>
+
+typedef enum e_boolean {FALSE, TRUE}	t_boolean;
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 32
